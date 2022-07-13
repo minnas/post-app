4c178952e60be1a7f9e4321649d1216abdf4c25b
Minna Salonen <minsalon@gmail.com>
2022-07-13 12:29:04 +0300

    added some bg images

--- src/components/tools/Palceholder.vue
+++ src/components/tools/Palceholder.vue
@@ -16,24 +16,34 @@ export default defineComponent({
 });
 </script>
 <style lang="css" scoed>
-.placeholder-container,
-.placeholder-container-inner {
-  width: 100%;
+
+.placeholder-container{
+  background-image: url("../../assets/placeholder.png");
+  background-repeat: no-repeat;
+  background-size: cover;
+  height: 6rem;
+  padding: 2rem 3rem;
+  width: inherit;
 }
+
 .placeholder-container-inner {
   display: flex;
   justify-content: center;
   align-items: center;
+  height: 100%;
   grid-column-gap: 2rem;
-  height: 6rem;
+  background-color: rgba(255, 255, 255, 0.6);
+  -webkit-backdrop-filter: blur(5px);
+  backdrop-filter: blur(5px);
+  border-radius: 8px;
 }
 .placeholder-icon {
-  font-size: 3rem;
+  font-size: 2rem;
   color: rgba(0, 0, 0, .8);
 }
 .placeholder-container-inner span {
   font-style: italic;
-  font-size: 1.25rem;
+  font-size: 1rem;
   color: rgba(0, 0, 0, .8);
 }
 
